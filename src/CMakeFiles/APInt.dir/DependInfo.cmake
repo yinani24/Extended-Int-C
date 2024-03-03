b0d1760485d23b6e6bf9e5ae45a602b72518e9d7
Yash <yinani@ucdavis.edu>
2024-03-03 01:16:28 -0800

    changes

---
+++ src/CMakeFiles/APInt.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/Yash Inani/OneDrive/Desktop/Extended-Int-C/src/lib/APInt.c" "/mnt/c/Users/Yash Inani/OneDrive/Desktop/Extended-Int-C/src/CMakeFiles/APInt.dir/lib/APInt.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/APInt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/APInt.dir/lib/APInt.c.o"
+  "libAPInt.pdb"
+  "libAPInt.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/APInt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
